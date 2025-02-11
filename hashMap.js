ce94454d7d94728443be42eda2f3c6b5721b553d
Gabriel <gabrielpachecotec@gmail.com>
2025-02-11 11:34:00 +0000

    Set loadFactor, capacity and add hash and updateCapacity functions

---
+++ hashMap.js
@@ -0,0 +1,26 @@
+function HashMap() {
+    const LoadFactor = 0.8;
+    let capacity = 16;
+    let buckets = [];
+
+    function updateCapacity(length) {
+        capacity = length;
+        buckets.length = length;
+    }
+
+    function hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % capacity;
+        }
+
+        return hashCode;
+    }
+
+    return {
+        buckets,
+    };
+}
