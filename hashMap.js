7834725dd76adaec10084e98c0519518b067e3d8
Gabriel <gabrielpachecotec@gmail.com>
2025-02-12 10:21:03 +0000

    Write set(key, value) function
    This function takes a key and a value, generates a hash value(index for the bucket in
    the hashtable), creates a linkedList in the generated index if there's none yet and
    appends the key/value pair passed. If there's already a linkedList(collision), it appends the
    key/value pair to the end of the list. Finally if the key passed is already present in
    any bucket, it only updates the value of it, since the keys are the same.
    
    After every insertion, we check if the number of buckets in the hashtable is above
    the loadfactor, if it is we duplicate the size of the hashtable and rehash the keys to
    insert them in new bucket indexes.

--- hashMap.js
+++ hashMap.js
@@ -1,13 +1,87 @@
-import { linkedList } from "./linkedList";
+import { linkedList } from "./linkedList.js";
 
 function HashMap() {
     const LoadFactor = 0.8;
     let capacity = 16;
-    let buckets = [];
+    let buckets = new Array(capacity);
+    let numOfBuckets = 0;
+    let numOfKeys = 0;
+
+    function incrementNumOfBucketsByOne() {
+        numOfBuckets++;
+    }
+
+    function incrementNumOfKeysByOne() {
+        numOfKeys++;
+    }
+
+    function getHashTable() {
+        return buckets;
+    }
 
     function updateCapacity(length) {
         capacity = length;
-        buckets.length = length;
+    }
+
+    function set(key, value) {
+        const keyValuePair = { [key]: value };
+        const bucketIndex = hash(key);
+        console.log(bucketIndex);
+
+        if (buckets[bucketIndex] === undefined) {
+            buckets[bucketIndex] = linkedList();
+            buckets[bucketIndex].append(keyValuePair);
+            incrementNumOfBucketsByOne();
+            incrementNumOfKeysByOne();
+            increaseHashTable();
+            return;
+        } else if (modifyValueIfKeyPresent(key, value, buckets[bucketIndex])) {
+            return;
+        }
+
+        buckets[bucketIndex].append(keyValuePair);
+        incrementNumOfKeysByOne();
+        increaseHashTable();
+    }
+
+    function modifyValueIfKeyPresent(key, value, bucket) {
+        let currentNode = bucket.getHead();
+
+        while (currentNode !== null) {
+            if (currentNode.value[key]) {
+                currentNode.value[key] = value;
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return false;
+    }
+
+    function increaseHashTable() {
+        if (!shouldWeIncreaseSizeOfHashTable()) {
+            return;
+        }
+
+        updateCapacity(capacity * 2);
+        const newHashTable = new Array(capacity);
+
+        for (let bucket of buckets) {
+            if (!bucket) {
+                continue;
+            }
+            const headOfBucket = bucket.getHead();
+            const headBucketKey = Object.keys(headOfBucket.value)[0];
+            const newIndex = hash(headBucketKey);
+            newHashTable[newIndex] = bucket;
+        }
+        buckets = newHashTable;
+    }
+
+    function shouldWeIncreaseSizeOfHashTable() {
+        const maxValue = Math.round(LoadFactor * capacity);
+
+        return numOfBuckets >= maxValue ? true : false;
     }
 
     function hash(key) {
@@ -23,6 +97,7 @@ function HashMap() {
     }
 
     return {
-        buckets,
+        getHashTable,
+        set,
     };
 }
